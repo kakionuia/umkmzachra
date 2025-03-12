06dd1887e6709d50ef40479313194b0a77ef8dd3
kakionuia <kosiandi6@gmail.com>
2025-03-12 07:31:03 +0000

    Create database.php

---
+++ database/database.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_form";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
